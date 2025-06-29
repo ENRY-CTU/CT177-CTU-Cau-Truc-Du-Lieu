2e37a8883480ac0a29e43ac1507302c738f3a316
ENRY-CTU <phucb2405049@student.ctu.edu.vn>
2025-06-29 18:50:57 +0700

    Create Khai_Báo.c

---
+++ Danh sách liên kết/Đa thức/Khai_Báo.c
@@ -0,0 +1,12 @@
+#Khai báo đơn thức
+typedef struct {
+    double he_so;
+    int bac;
+} DonThuc;
+#Khai báo đa thức
+struct Node{
+    DonThuc e;
+    struct Node* Next;
+};
+typedef struct Node* Position;
+typedef Position DaThuc;
