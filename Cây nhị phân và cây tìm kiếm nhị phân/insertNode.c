57116a46920d53b9505007c98ca9667afce6770c
ENRY-CTU <phucb2405049@student.ctu.edu.vn>
2025-07-15 21:34:25 +0700

    Update insertNode.c

--- Cây nhị phân và cây tìm kiếm nhị phân/insertNode.c
+++ Cây nhị phân và cây tìm kiếm nhị phân/insertNode.c
@@ -1,17 +1,8 @@
-int member(int x, Tree T) {
-    if (T == NULL) return 0;
-    if (T->Key == x) return 1;
-    if (x < T->Key) return member(x, T->Left);
-    else return member(x, T->Right);
-}
 void insertNode(int x, Tree *T){
     if (*T == NULL) {
         *T = (Tree)malloc(sizeof(struct Node));
         (*T)->Key = x;
         (*T)->Left = (*T)->Right = NULL;
-    if(!member(x, *T)) {
-        return;
-    }
     } else if (x < (*T)->Key) {
         insertNode(x, &((*T)->Left));
     } else if (x > (*T)->Key) {
