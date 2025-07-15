ca95e9672bbcd9e14edbec2c6d07b965c0ab4ec4
ENRY-CTU <phucb2405049@student.ctu.edu.vn>
2025-07-15 21:43:57 +0700

    Update getNext.c

--- Cây nhị phân và cây tìm kiếm nhị phân/getNext.c
+++ Cây nhị phân và cây tìm kiếm nhị phân/getNext.c
@@ -11,3 +11,13 @@ Tree getNext(int x, Tree T) {
     }
     return q;
 }
+//
+Tree getNext(int x, Tree T){
+    if(T == NULL) return NULL;
+    if(T->Key <= x) {
+        return getNext(x, T->Right);
+    } else {
+        Tree left = getNext(x, T->Left);
+        return (left != NULL) ? left : T;
+    }
+}
