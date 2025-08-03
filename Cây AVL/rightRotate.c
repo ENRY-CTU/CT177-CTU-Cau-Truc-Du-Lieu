76da1bbb8fb21ce948148ca73becfa5d3add9d10
ENRY-CTU <phucb2405049@student.ctu.edu.vn>
2025-08-03 20:09:03 +0700

    Create rightRotate.c

---
+++ Cây AVL/rightRotate.c
@@ -0,0 +1,12 @@
+Tree rightRotate(Tree y) {
+    Tree x = y->left;
+    Tree T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + (height(y->left) > height(y->right) ? height(y->left) : height(y->right));
+    x->height = 1 + (height(x->left) > height(x->right) ? height(x->left) : height(x->right));
+
+    return x;
+}
