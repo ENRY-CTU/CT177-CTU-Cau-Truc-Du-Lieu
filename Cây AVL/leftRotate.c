9e6a150c03bfc6be91bd1d2fa24ffc7971c6b065
ENRY-CTU <phucb2405049@student.ctu.edu.vn>
2025-08-03 20:08:50 +0700

    Create leftRotate.c

---
+++ Cây AVL/leftRotate.c
@@ -0,0 +1,12 @@
+Tree leftRotate(Tree x) {
+    Tree y = x->right;
+    Tree T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + (height(x->left) > height(x->right) ? height(x->left) : height(x->right));
+    y->height = 1 + (height(y->left) > height(y->right) ? height(y->left) : height(y->right));
+
+    return y;
+}
